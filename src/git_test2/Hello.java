41caf267b2dfcf29fac1dddf9b8d0ab3037cff0f
yunaluv358 <yunaluv358@gmail.com>
2020-10-10 02:06:02 +0900

    메시지 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,4 +2,8 @@ package git_test2;
 
 public class Hello {
 
+    public void say() {
+        System.out.println("æ»≥Á«œººø‰");
+    }
+
 }
--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -2,4 +2,8 @@ package git_test2;
 
 public class Main {
 
+    public static void main(String[] args) {
+        new Hello().say();
+    }
+
 }
