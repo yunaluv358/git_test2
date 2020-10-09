319c9d7c44dec5282d446359b1c86d19e8c349bf
yunaluv358 <yunaluv358@gmail.com>
2020-10-10 03:58:47 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,8 +2,18 @@ package git_test2;
 
 public class Hello {
 
+    String message;
+
+    public Hello() {
+        this("æ»≥Á«œººø‰");
+    }
+
+    public Hello(String message) {
+        this.message = message;
+    }
+
     public void say() {
-        System.out.println("æ»≥Á«œººø‰");
+        System.out.println(message);
     }
 
 }
